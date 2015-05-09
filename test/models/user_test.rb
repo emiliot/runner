edbc099c0be934e6937f8cd44047717e06806194
Emilio Tirado <emilio.tirado57@gmail.com>
2015-05-09 15:55:58 -0430

    model tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without name" do
+    user = User.new
+    user.password = "123456"
+	user.email = "emilio.tirado57@gmail.com"
+    assert_not user.save, "Saved the user without name"
+  end
+
+  test "should not save user without password" do
+  	user = User.new
+  	user.name = "Juanito"
+	user.email = "emilio.tirado57@gmail.com"
+  	assert_not user.save, "Saved user without password"
+  end
+
+  test "should not save user without email" do
+  	user = User.new
+  	user.name = "Juanito"
+  	user.password = "123456"
+  	assert_not user.save, "Saved user without email"
+  end
 end
