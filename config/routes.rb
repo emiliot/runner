dcb334a90ac0fd22c27f7df1444f7180d34f558f
Emilio Tirado <emilio.tirado57@gmail.com>
2015-05-09 20:07:19 -0430

    starting user controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-    resources :users
+    resources :users, :defaults => { :format => :json }, :only => [:index, :show, :update, :destroy]
 
   # Example resource route with options:
   #   resources :products do
