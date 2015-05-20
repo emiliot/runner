1f78ec9a85ad701fb8d83a15105c3ddaef67db6f
Emilio Tirado <emilio.tirado57@gmail.com>
2015-05-20 19:18:45 -0430

    building states

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: '/auth'
   scope '/api/v1' do
-    end
+    mount_devise_token_auth_for 'User', at: '/auth'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,32 +1,55 @@
 'use strict';
 
-angular.module('frontend', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap', 'rails', 'ng-token-auth'])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      })
-      .state('users', {
-      	url: '/users',
-      	templateUrl : 'app/components/users/users.html',
-      	controller: 'UserCtrl'
-      })
-      .state('login', {
-        url: '/login',
-        templateUrl : 'app/components/users/login.html',
-        controller: 'UserLoginCtrl'
-      })
-      .state('register', {
-        url: '/registers',
-        templateUrl : 'app/components/register/registers.html',
-        controller: 'RegisterCtrl'
-      });
+angular.module('frontend', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 
+	'ngResource', 'ui.router', 'ui.bootstrap', 'rails', 'ng-token-auth'])
+.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+	$stateProvider
 
-    // use the HTML5 History API
-    $locationProvider.html5Mode(true);
+	.state('home', {
+		url: '/',
+		templateUrl: 'app/main/main.html',
+		controller: 'MainCtrl'
+	})
+	.state('login', {
+		url: '/login',
+		templateUrl : 'app/components/users/login.html',
+		controller: 'UserLoginCtrl'
+	})
+	.state('register', {
+		url: '/registers',
+		templateUrl : 'app/components/register/registers.html',
+		controller: 'RegisterCtrl'
+	})
 
-    $urlRouterProvider.otherwise('/');
-  })
-;
+	.state('runner', {
+		url: '/runner',
+		abstract: true,
+		template: '<ui-view/>',
+		resolve: {
+			auth: function($auth){
+				return $auth.validateUser();
+			}
+		}
+	})
+	.state('admin', {
+		url: '/admin',
+		templateUrl: 'app/components/users/users.html',
+		controller: 'UserCtrl'
+	})
+	.state('runner.runs', {
+		url: '/runs',
+		templateUrl: 'app/components/runs/runs.html',
+	})
+	.state('runner.account', {
+		url: '/account',
+		templateUrl: 'app/components/users/account.html'
+	});
+
+	$locationProvider.html5Mode(true);
+	$urlRouterProvider.otherwise('/');
+
+}).config(function($authProvider){
+	$authProvider.configure({
+		apiUrl: 'http://localhost:3000'
+	});
+});
