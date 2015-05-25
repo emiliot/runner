0fd8af1e562d1f4171e96101a07e2bfa59c823f9
Emilio Tirado <emilio.tirado57@gmail.com>
2015-05-25 06:50:27 -0430

    listing users in admin view

--- frontend/src/app/components/admin/admin.controller.js
+++ frontend/src/app/components/admin/admin.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('frontend.admin')
-.controller('AdminCtrl', ['$scope', function($scope){
-	console.log('AdminCtrl');
+.controller('AdminCtrl', ['$scope', 'User',  function($scope, User){
+	$scope.users = [];
+
+	User.query(function(data){
+		$scope.users = data;
+		console.log(data);
+	});
 }]);
