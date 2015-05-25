117ad1dd18fd37af7aed40350ad88ca1a098dbfe
Emilio Tirado <emilio.tirado57@gmail.com>
2015-05-25 03:34:42 -0430

    update run

--- frontend/src/app/components/runner/runner.controller.js
+++ frontend/src/app/components/runner/runner.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('frontend.runner')
-.controller('RunnerCtrl', ['$scope', 'Run',  function($scope, Run){
+.controller('RunnerCtrl', ['$scope', 'Run', '$modal',  function($scope, Run, $modal){
 
 	$scope.open = function($event) {
 		$event.preventDefault();
@@ -24,11 +24,9 @@ angular.module('frontend.runner')
 
 	Run.query(function(data){
 		$scope.runs = data;
-		console.log(data);
 	});
 
 	$scope.create = function(run){
-		console.log(run);
 		run.time_run = (run.time_run_h * 60) + run.time_run_m;
 		var nextRun = new Run(run);
 		nextRun.$save(function(data){
@@ -50,6 +48,36 @@ angular.module('frontend.runner')
 	};
 
 	$scope.edit = function(run){
-		console.log(run);
-	}
+		run.time_run_h = parseInt(run.time_run / 60);
+		run.time_run_m = run.time_run % 60;
+
+		var modalInstance = $modal.open({
+			animation: true,
+			templateUrl: 'app/components/runner/modal.html',
+			controller: 'ModalInstanceCtrl',
+			resolve: {
+				run: function () {
+					return run;
+				}
+			}
+		});
+
+		modalInstance.result.then(function (updatedRun) {
+			updatedRun.$update(function(data){
+			}, function(data){
+				console.log('error');
+			});
+		});
+	};
+}])
+.controller('ModalInstanceCtrl', ['$scope', '$modalInstance', 'run', function($scope, $modalInstance, run){
+	$scope.run = run;
+
+	$scope.ok = function () {
+	  $modalInstance.close($scope.run);
+	};
+
+	$scope.cancel = function () {
+	  $modalInstance.dismiss('cancel');
+	};
 }]);
