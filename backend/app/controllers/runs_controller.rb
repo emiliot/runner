703b4d0209ed72b6d57448ee43c4629a3e94e3c0
Emilio Tirado <emilio.tirado57@gmail.com>
2015-05-24 19:57:03 -0430

    working on runs controller

--- backend/app/controllers/runs_controller.rb
+++ backend/app/controllers/runs_controller.rb
@@ -1,59 +1,59 @@
 class RunsController < ApplicationController
-  before_action :set_run, only: [:show, :update, :destroy]
+	before_action :set_run, only: [:show, :update, :destroy]
 
-  # GET /runs
-  # GET /runs.json
-  def index
-    @runs = Run.all
+	# GET /runs
+	# GET /runs.json
+	def index
+		@runs = Run.all
 
-    render json: @runs
-  end
+		render json: @runs
+	end
 
-  # GET /runs/1
-  # GET /runs/1.json
-  def show
-    render json: @run
-  end
+	# GET /runs/1
+	# GET /runs/1.json
+	def show
+		render json: @run
+	end
 
-  # POST /runs
-  # POST /runs.json
-  def create
-    @run = Run.new(run_params)
+	# POST /runs
+	# POST /runs.json
+	def create
+		@run = Run.new(run_params)
 
-    if @run.save
-      render json: @run, status: :created, location: @run
-    else
-      render json: @run.errors, status: :unprocessable_entity
-    end
-  end
+		if @run.save
+			render json: @run, status: :created
+		else
+			render json: @run.errors, status: :unprocessable_entity
+		end
+	end
 
-  # PATCH/PUT /runs/1
-  # PATCH/PUT /runs/1.json
-  def update
-    @run = Run.find(params[:id])
+	# PATCH/PUT /runs/1
+	# PATCH/PUT /runs/1.json
+	def update
+		@run = Run.find(params[:id])
 
-    if @run.update(run_params)
-      head :no_content
-    else
-      render json: @run.errors, status: :unprocessable_entity
-    end
-  end
+		if @run.update(run_params)
+			render json: @run, status: :ok
+		else
+			render json: @run.errors, status: :unprocessable_entity
+		end
+	end
 
-  # DELETE /runs/1
-  # DELETE /runs/1.json
-  def destroy
-    @run.destroy
+	# DELETE /runs/1
+	# DELETE /runs/1.json
+	def destroy
+		@run.destroy
 
-    head :no_content
-  end
+		head :no_content
+	end
 
-  private
+	private
 
-    def set_run
-      @run = Run.find(params[:id])
-    end
+	def set_run
+		@run = Run.find(params[:id])
+	end
 
-    def run_params
-      params[:run]
-    end
+	def run_params
+		params.permit(:date_run, :time_run, :distance_run, :user_id)
+	end
 end
